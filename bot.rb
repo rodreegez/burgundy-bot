3a28c966b94b02b93279f564fe43a6e101cedd99
Adam <electronicbattleweapon7@gmail.com>
2014-06-11 00:27:35 +0100

    do both, half the time

--- bot.rb
+++ bot.rb
@@ -5,10 +5,14 @@ load 'twitter_auth.rb'
 require 'tweetbot'
 
 bot = TweetBot.configure do |config|
-  config.response_frequency = 100
+  config.response_frequency = 50
 
   config.respond_to_phrase "Milk was a bad choice" do |responses|
     responses << "It's so damn hot!"
+  end
+
+  config.respond_to_phrase "It's so damn hot!" do |responses|
+    responses << "Milk was a bad choice."
   end
 
   config.twitter_auth = TwitterAuth::AuthKeys
