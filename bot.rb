cb6a8b05d01d47c01c93c1acb9515a531dafba8e
Adam <electronicbattleweapon7@gmail.com>
2014-06-10 22:03:11 +0100

    Milk was a bad choice

--- bot.rb
+++ bot.rb
@@ -1,3 +1,6 @@
+# encoding: UTF-8
+
+$stdout.sync = true
 load 'twitter_auth.rb'
 require 'tweetbot'
 
@@ -11,6 +14,8 @@ bot = TweetBot.configure do |config|
   config.respond_to_phrase "Milk was a bad choice" do |responses|
     responses << "It's so damn hot!"
   end
+
+  config.twitter_auth = TwitterAuth::AuthKeys
 end
 
 bot.talk
