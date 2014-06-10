8155c5fc85a56591b05b0999c2fa90ae7b4f68b2
Adam <electronicbattleweapon7@gmail.com>
2014-06-10 21:29:48 +0100

    It's so damn hot

---
+++ twitter_auth.rb
@@ -0,0 +1,14 @@
+module TwitterAuth
+  MyName = 'BurgundyBot'
+  ApigeeEnpoint = nil
+  def self.use_apigee?
+    !ApigeeEnpoint.nil?
+  end
+  AuthKeys = {
+    :consumer_key => ENV['TWITTER_CONSUMER_KEY'],
+    :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'],
+    :oauth_token => ENV['TWITTER_OAUTH_TOKEN'],
+    :oauth_token_secret => ENV['TWITTER_OAUTH_TOKEN_SECRET']
+  }
+end
+
