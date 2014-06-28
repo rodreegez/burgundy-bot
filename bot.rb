d6df740bbc6b1b38d087f189cb1d94e8673a54d8
Adam <electronicbattleweapon7@gmail.com>
2014-06-28 14:32:00 +0100

    less is more

--- bot.rb
+++ bot.rb
@@ -5,7 +5,7 @@ load 'twitter_auth.rb'
 require 'tweetbot'
 
 bot = TweetBot.configure do |config|
-  config.response_frequency = 10
+  config.response_frequency = 5
 
   config.respond_to_phrase "Milk was a bad choice" do |responses|
     responses << "It's so damn hot!"
